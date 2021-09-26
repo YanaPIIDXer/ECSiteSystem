d50697bcc26e224042bbe95b1f84c0f65cdf31ca
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 07:47:58 +0900

    ヘッダモジュールをとりあえず定義

--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopComponent } from './pages/top/top.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
