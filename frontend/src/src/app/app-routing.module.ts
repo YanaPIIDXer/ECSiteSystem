b9a2fb7e0e7222e157cbf80bd265f7b8fb77808f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 07:37:54 +0900

    ページモジュール用のディレクトリを掘った

--- frontend/src/src/app/app-routing.module.ts
+++ frontend/src/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TopComponent } from './top/top.component';
+import { TopComponent } from './pages/top/top.component';
 
 const routes: Routes = [
   { path: "", component: TopComponent }
--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopComponent } from './top/top.component';
+import { TopComponent } from './pages/top/top.component';
 
 @NgModule({
   declarations: [
