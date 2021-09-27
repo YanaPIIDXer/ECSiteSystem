5680d9a790d49df7a2b3caadd53e64264c0b7a4c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 04:35:10 +0900

    環境変数からシークレットキーを放り込むようにした

--- backend/ecsite/src/main/java/com/yanap/ecsite/EcsiteApplication.java
+++ backend/ecsite/src/main/java/com/yanap/ecsite/EcsiteApplication.java
@@ -1,5 +1,7 @@
 package com.yanap.ecsite;
 
+import com.stripe.Stripe;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcsiteApplication {
 
 	public static void main(String[] args) {
+		Stripe.apiKey = System.getenv("STRIPE_SECRET_KEY");
 		SpringApplication.run(EcsiteApplication.class, args);
 	}
 
