028d4c68e0c5fd407d9ed9ef69ef42c2123bcbde
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 05:20:00 +0900

    認証用ユーザクラス定義

---
+++ backend/ecsite/src/main/java/com/yanap/ecsite/auth/AuthUser.java
@@ -0,0 +1,59 @@
+package com.yanap.ecsite.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.yanap.ecsite.entity.User;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+// 認証ユーザ
+public class AuthUser implements UserDetails {
+
+    private User user;
+
+    public AuthUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> list = new ArrayList<>();
+        list.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return list;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+}
