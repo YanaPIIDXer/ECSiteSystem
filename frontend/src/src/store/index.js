2e74a168452e9995ffdf41d2a158320e326b9d8f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 20:00:43 +0900

    認証状態のステート取得や操作関係の実装

--- frontend/src/src/store/index.js
+++ frontend/src/src/store/index.js
@@ -2,11 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    role: "",
+  },
+  getters: {
+    isLogin: (state) => (state.role != ""),
+    isAdmin: (state) => (state.role != "ADMIN"),
   },
   mutations: {
+    loginAsUser: (state) => (state.role = "USER"),
+    loginAsAdmin: (state) => (state.role = "ADMIN"),
+    logout: (state) => (state.role = ""),
   },
-  actions: {
-  },
-  modules: {
-  }
 })
