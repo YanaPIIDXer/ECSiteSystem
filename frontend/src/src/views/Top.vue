b802f30a4c21eb39b22d8dcdd07312558bebdd14
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 08:13:01 +0900

    商品一覧実装

--- frontend/src/src/views/Top.vue
+++ frontend/src/src/views/Top.vue
@@ -1,11 +1,37 @@
 <template>
     <div>
         <h1>トップページ</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-3" v-for="item in list" :key="item.id">
+                    <div class="card">
+                        <img :src="item.imageUrl" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ item.name }}</h5>
+                            <p class="card-text">{{ item.description }}</p>
+                            <button class="btn btn-primary">カートに入れる</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { get } from '../modules/APIConnection';
+
 export default {
-    name: "Top"
+    name: "Top",
+    data: function () {
+        return {
+            list: [],
+        };
+    },
+    mounted: async function () {
+        const res = await get("/product/list");
+        if (res.status != 200) { return; }
+        this.list = res.json;
+    }
 }
 </script>
