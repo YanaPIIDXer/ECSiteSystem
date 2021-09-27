18525940564cb08845ca7b3a7dc86ab38a1517d1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 00:48:30 +0900

    非ログイン状態のメニューを別Componentに切り出した

--- frontend/src/src/components/navbar/NavBar.vue
+++ frontend/src/src/components/navbar/NavBar.vue
@@ -9,11 +9,7 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 </ul>
                 <span v-if="!$store.getters.isLogin">
-                    <ul class="navbar-nav d-flex">
-                        <li class="nav-item nav-link">会員ログイン</li>
-                        <li class="nav-item nav-link">新規登録</li>
-                        <li class="nav-item"><router-link to="/login_admin"><span class="nav-link">管理者ログイン</span></router-link></li>
-                    </ul>
+                    <nav-bar-not-login />
                 </span>
                 <span v-else>
                     <div class="navbar-nav">
@@ -27,9 +23,13 @@
 
 <script>
 import { get } from '../../modules/APIConnection';
+import NavBarNotLogin from './NavBarNotLogIn';
 
 export default {
     name: "NavBar",
+    components: {
+        NavBarNotLogin
+    },
     methods: {
         logout: async function () {
             if (!confirm("ログアウトしますか？")) { return; }
---
+++ frontend/src/src/components/navbar/NavBarNotLogIn.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="navbar-nav d-flex">
+        <li class="nav-item nav-link">会員ログイン</li>
+        <li class="nav-item nav-link">新規登録</li>
+        <li class="nav-item"><router-link to="/login_admin"><span class="nav-link">管理者ログイン</span></router-link></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "NavBarNotLogin"
+}
+</script>
