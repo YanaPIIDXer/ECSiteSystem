c5e0d877ce53f9357f1543c80d50d930b2bdc7ad
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 07:16:23 +0900

    商品エンティティ定義

---
+++ backend/ecsite/src/main/java/com/yanap/ecsite/entity/Product.java
@@ -0,0 +1,25 @@
+package com.yanap.ecsite.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// 商品エンティティ
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private int price;
+    private String imageUrl;
+    private String description;
+}
