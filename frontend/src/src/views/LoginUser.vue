4134b2cb4bdf287c9b2c97f97ff60cf66a7a6779
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 05:41:15 +0900

    ログイン処理実装

--- frontend/src/src/views/LoginUser.vue
+++ frontend/src/src/views/LoginUser.vue
@@ -2,11 +2,40 @@
     <div>
         <h1>ログイン</h1>
         <hr />
+        <div>
+            <label class="form-label">メールアドレス</label>
+            <input type="text" class="form-control" v-model="email" />
+            <label class="form-label">パスワード</label>
+            <input type="password" class="form-control" v-model="password" />
+            <button @click="login">ログイン</button>
+        </div>
     </div>
 </template>
 
 <script>
+import { post } from '../modules/APIConnection';
+
 export default {
     name: "LoginUser",
+    data: function () {
+        return {
+            email: "",
+            password: "",
+        };
+    },
+    methods: {
+        login: async function () {
+            var params = new URLSearchParams();
+            params.append("email", this.email);
+            params.append("password", this.password);
+            const res = await post("/user/login", params);
+            if (res.status != 200) {
+                alert("ログインに失敗しました");
+                return;
+            }
+            this.$store.commit("loginAsUser", "dummy");
+            this.$router.push("/");
+        }
+    }
 }
 </script>
