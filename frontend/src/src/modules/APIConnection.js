57ea807c2bfa341dc80b59d3134b2e46b0ed39cb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 23:15:57 +0900

    API実験プロジェクトから失敬

---
+++ frontend/src/src/modules/APIConnection.js
@@ -0,0 +1,37 @@
+import conn from "./BasicConnection";
+
+const ENDPOINT = "http://localhost:3000/";
+
+export async function get(api) {
+    const url = ENDPOINT + api;
+    var json = null;
+    try {
+        const result = await conn.get(url);
+        if (result.status != 200) {
+            alert("StatusCode:" + result.status);
+            return null;
+        }
+        json = JSON.parse(JSON.stringify(result.data));    
+    } catch (e) {
+        alert(e);
+        return null;
+    }
+    return json;
+}
+
+export async function post(api, params) {
+    const url = ENDPOINT + api;
+    var json = null;
+    try {
+        const result = await conn.post(url, params);
+        if (result.status != 200) {
+            alert("StatusCode:" + result.status);
+            return null;
+        }
+        json = JSON.parse(JSON.stringify(result.data));            
+    } catch (e) {
+        alert(e);
+        return null;
+    }
+    return json;
+}
