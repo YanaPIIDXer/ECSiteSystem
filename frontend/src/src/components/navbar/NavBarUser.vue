f18eac705230b8000bde2c1a91fdedc2a0083f70
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 05:42:27 +0900

    とりあえず管理者の方から失敬して必要な個所を修正

--- frontend/src/src/components/navbar/NavBarUser.vue
+++ frontend/src/src/components/navbar/NavBarUser.vue
@@ -1,10 +1,31 @@
 <template>
-    <div>
+    <div class="navbar-nav">
+        <span class="nav-item nav-link" @click="logout">ログアウト</span>
     </div>
 </template>
 
 <script>
+import { get } from '../../modules/APIConnection';
+
 export default {
-    name: "NavBarUser"
+    name: "NavBarUser",
+    methods: {
+        logout: async function () {
+            if (!confirm("ログアウトしますか？")) { return; }
+            const res = await get("/user/logout");
+            if (res.status != 200) {
+                alert("ログアウトに失敗しました");
+                return;
+            }
+            this.$store.commit("logout");
+            this.$router.push("/");
+        }
+    }
 }
 </script>
+
+<style scoped>
+span .nav-link {
+    cursor: pointer;
+}
+</style>
