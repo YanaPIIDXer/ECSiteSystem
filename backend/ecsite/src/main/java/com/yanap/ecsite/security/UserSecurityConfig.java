3857441b0c6043e04c7f4f4a3c1edabf4d91c253
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 23:52:12 +0900

    先にある程度実装

--- backend/ecsite/src/main/java/com/yanap/ecsite/security/UserSecurityConfig.java
+++ backend/ecsite/src/main/java/com/yanap/ecsite/security/UserSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 // ユーザセキュリティ構成
 @Configuration
@@ -15,6 +20,25 @@ public class UserSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.mvcMatcher("/user/**")
                 .authorizeRequests()
-                    .anyRequest().hasRole("USER");
+                    .anyRequest().hasRole("USER")
+            .and()
+                .formLogin()
+                    .loginProcessingUrl("/user/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .successHandler(new SimpleUrlAuthenticationSuccessHandler())
+                    .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                    .permitAll()
+            .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                    .logoutSuccessHandler(new SimpleUrlLogoutSuccessHandler())
+            .and()
+                .csrf().disable().cors();
+    }
+    
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // TODO:実装
     }
 }
