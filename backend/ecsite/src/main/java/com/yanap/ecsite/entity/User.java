183ed184c8bf8c9de36cb71d73c03deeee538c9f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 04:42:13 +0900

    ユーザエンティティ定義

---
+++ backend/ecsite/src/main/java/com/yanap/ecsite/entity/User.java
@@ -0,0 +1,31 @@
+package com.yanap.ecsite.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// ユーザエンティティ
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    private String stripeId;
+}
