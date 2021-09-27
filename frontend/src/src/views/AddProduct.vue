2ce517c9d8db6774f661ed18fad94076694056b1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 07:30:39 +0900

    商品追加処理

--- frontend/src/src/views/AddProduct.vue
+++ frontend/src/src/views/AddProduct.vue
@@ -2,11 +2,49 @@
     <div>
         <h1>商品追加</h1>
         <hr />
+        <label class="form-label">商品名</label>
+        <input type="text" class="form-control" v-model="name" />
+        <label class="form-label">値段</label>
+        <input type="number" class="form-control" v-model="price" />
+        <label class="form-label">画像URL</label>
+        <input type="text" class="form-control" v-model="imageUrl" />
+        <label class="form-label">商品概要</label>
+        <textarea class="form-control" v-model="description"></textarea>
+        <button @click="add">追加</button>
     </div>
 </template>
 
 <script>
+import { post } from '../modules/APIConnection';
+
 export default {
     name: "AddProduct",
+    data: function () {
+        return {
+            name: "",
+            price: 0,
+            imageUrl: "",
+            description: "",
+        };
+    },
+    methods: {
+        add: async function () {
+            if (!confirm("追加しますか？")) { return; }
+
+            var params = new URLSearchParams();
+            params.append("name", this.name);
+            params.append("price", this.price);
+            params.append("imageUrl", this.imageUrl);
+            params.append("description", this.description);
+            const res = await post("/admin/product/add", params);
+            if (res.status != 200) {
+                alert("追加に失敗しました");
+                return;
+            }
+            
+            alert("追加しました");
+            this.$router.push("/admin");
+        }
+    }
 }
 </script>
