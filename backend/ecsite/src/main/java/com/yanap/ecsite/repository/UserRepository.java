7aa60e8c4d3b48f1494a9865c72a172232659a80
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 04:46:06 +0900

    アノテーション付け忘れ

--- backend/ecsite/src/main/java/com/yanap/ecsite/repository/UserRepository.java
+++ backend/ecsite/src/main/java/com/yanap/ecsite/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.yanap.ecsite.repository;
 import com.yanap.ecsite.entity.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
