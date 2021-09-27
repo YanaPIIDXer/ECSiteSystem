9e32f0c1cbbabc80605edc94596e24818661d55c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 08:55:15 +0900

    改行対応

--- frontend/src/src/components/ProductCard.vue
+++ frontend/src/src/components/ProductCard.vue
@@ -3,7 +3,7 @@
         <img :src="imageUrl" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ name }}</h5>
-            <p class="card-text">{{ description }}</p>
+            <p class="card-text" v-html="dispDesc"></p>
             <button class="btn btn-primary">カートに入れる</button>
         </div>
     </div>
@@ -17,5 +17,10 @@ export default {
         "imageUrl",
         "description"
     ],
+    data: function () {
+        return {
+            dispDesc: this.description.replace("\n", "<br />")
+        };
+    }
 }
 </script>
