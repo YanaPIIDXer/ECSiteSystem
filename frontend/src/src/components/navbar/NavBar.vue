7418c89ef687ae02ad5dae82d4b7c279b6db415b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 18:07:40 +0900

    とりあえず公式のものを失敬してカスタマイズした

--- frontend/src/src/components/navbar/NavBar.vue
+++ frontend/src/src/components/navbar/NavBar.vue
@@ -1,7 +1,19 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <p class="navbar-brand">通販システム</p>
-        <div class="navbar-nav">
+        <div class="container-fluid">
+            <span class="navbar-brand">通販システム</span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                </ul>
+                <ul class="navbar-nav d-flex">
+                    <li class="nav-item nav-link">会員ログイン</li>
+                    <li class="nav-item nav-link">新規登録</li>
+                    <li class="nav-item nav-link">管理者ログイン</li>
+                </ul>
+            </div>
         </div>
     </nav>
 </template>
