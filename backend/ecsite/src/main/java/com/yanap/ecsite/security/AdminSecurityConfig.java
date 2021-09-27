c6fdd01d1d8ca6f8014606ce46b4a8cc08b48794
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-27 23:35:39 +0900

    管理者ログイン処理

--- backend/ecsite/src/main/java/com/yanap/ecsite/security/AdminSecurityConfig.java
+++ backend/ecsite/src/main/java/com/yanap/ecsite/security/AdminSecurityConfig.java
@@ -1,10 +1,23 @@
 package com.yanap.ecsite.security;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 // 管理者セキュリティ構成
 @Configuration
@@ -15,6 +28,42 @@ public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.mvcMatcher("/admin/**")
                 .authorizeRequests()
-                    .anyRequest().hasRole("ADMIN");
+                    .anyRequest().hasRole("ADMIN")
+            .and()
+                .formLogin()
+                    .loginProcessingUrl("/admin/login")
+                    .usernameParameter("name")
+                    .passwordParameter("password")
+                    .successHandler(new SimpleUrlAuthenticationSuccessHandler())
+                    .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                    .permitAll()
+            .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout"))
+                    .logoutSuccessHandler(new SimpleUrlLogoutSuccessHandler())
+            .and()
+                .csrf().disable().cors();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("password")).roles("ADMIN");
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+    
+    // 参考：https://sebenkyo.com/2020/11/16/post-1649/
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost"));       // TODO:本番環境対応
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
