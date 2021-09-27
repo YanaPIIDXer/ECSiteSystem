ecbe6564894762af2ab64b7e75295ded13e6c3aa
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-09-28 00:06:18 +0900

    ログアウト実装

--- frontend/src/src/components/navbar/NavBar.vue
+++ frontend/src/src/components/navbar/NavBar.vue
@@ -8,19 +8,41 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 </ul>
-                <ul class="navbar-nav d-flex">
-                    <li class="nav-item nav-link">会員ログイン</li>
-                    <li class="nav-item nav-link">新規登録</li>
-                    <li class="nav-item"><router-link to="/login_admin"><span class="nav-link">管理者ログイン</span></router-link></li>
-                </ul>
+                <span v-if="!$store.getters.isLogin">
+                    <ul class="navbar-nav d-flex">
+                        <li class="nav-item nav-link">会員ログイン</li>
+                        <li class="nav-item nav-link">新規登録</li>
+                        <li class="nav-item"><router-link to="/login_admin"><span class="nav-link">管理者ログイン</span></router-link></li>
+                    </ul>
+                </span>
+                <span v-else>
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" href="#" @click="logout">ログアウト</a>
+                    </div>
+                </span>
             </div>
         </div>
     </nav>
 </template>
 
 <script>
+import { get } from '../../modules/APIConnection';
+
 export default {
-    name: "NavBar"
+    name: "NavBar",
+    methods: {
+        logout: async function () {
+            if (!confirm("ログアウトしますか？")) { return; }
+            const api = this.$store.getters.isUser ? "/user/logout" : "/admin/logout";
+            const res = await get(api);
+            if (res.status != 200) {
+                alert("ログアウトに失敗しました");
+                return;
+            }
+            this.$store.commit("logout");
+            this.$router.push("/");
+        }
+    }
 }
 </script>
 
