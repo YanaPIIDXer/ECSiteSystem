a7ea3c6d30b5c78e1e960f5af74beef75663b0ba
YanaPIIDXer <>
2021-09-29 18:16:31 +0900

    注文履歴エンティティ追加

--- backend/ecsite/src/main/java/com/yanap/ecsite/entity/User.java
+++ backend/ecsite/src/main/java/com/yanap/ecsite/entity/User.java
@@ -1,10 +1,13 @@
 package com.yanap.ecsite.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -30,4 +33,7 @@ public class User {
     private String address;
 
     private String stripeId;
+
+    @OneToMany
+    private List<Histroy> histories;
 }
