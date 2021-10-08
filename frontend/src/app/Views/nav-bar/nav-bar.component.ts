cb4e64013ca63309ac3bf9029196d14a904d8824
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-09 06:56:37 +0900

    とりあえずログインしているか・しているならロールの判定

--- frontend/src/app/Views/nav-bar/nav-bar.component.ts
+++ frontend/src/app/Views/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogInService } from 'src/app/Services/log-in.service';
 
 @Component({
   selector: 'nav-bar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public logInService: LogInService) { }
 
   ngOnInit(): void {
   }
