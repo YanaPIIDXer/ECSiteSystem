3dab545788dc4f2ea0803f0e323216c303f08b4a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-09 07:08:33 +0900

    とりあえず各状態におけるナビゲーションバー用Componentを先に定義しておく

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TopViewComponent } from './Views/top-view/top-view.component';
 import { NavBarComponent } from './Views/nav-bar/nav-bar.component';
+import { NavBarNotLogInComponent } from './Components/NavBar/nav-bar-not-log-in/nav-bar-not-log-in.component';
+import { NavBarAdminComponent } from './Components/NavBar/nav-bar-admin/nav-bar-admin.component';
+import { NavBarUserComponent } from './Components/NavBar/nav-bar-user/nav-bar-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopViewComponent,
-    NavBarComponent
+    NavBarComponent,
+    NavBarNotLogInComponent,
+    NavBarAdminComponent,
+    NavBarUserComponent
   ],
   imports: [
     BrowserModule,
