84bfc194b15188c4370dc57828b2b5b678efb5b4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-09 06:44:40 +0900

    トップページのComponentをとりあえず定義してルーティング

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { TopViewComponent } from './Views/top-view/top-view.component';
+
 const routes: Routes = [
-  
+  { path: "", component: TopViewComponent },
 ];
 
 @NgModule({
