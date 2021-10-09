6401372ffecb6b6104dcdf7a772dc743cbe6ea12
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 08:35:25 +0900

    購入処理実装

--- frontend/src/app/Views/cart/cart.component.ts
+++ frontend/src/app/Views/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartProduct } from 'src/app/Models/cart-product';
 import conn from '../../Modules/apiconnection';
 
@@ -12,7 +13,7 @@ export class CartComponent implements OnInit {
   list: CartProduct[]
   totalPrice: number
 
-  constructor() {
+  constructor(private router: Router) {
     this.list = [];
     this.totalPrice = 0;
   }
@@ -25,6 +26,19 @@ export class CartComponent implements OnInit {
     for (let item of this.list) {
       this.totalPrice += item.price * item.count;
     }
+  }
+
+  async buy(): Promise<void> {
+    if (this.totalPrice == 0) { return; }
+    if (!confirm("購入しますか？")) { return; }
+
+    const res = await conn.post("/user/cart/buy", new URLSearchParams());
+    if (res.status != 200 || !res.json.result) {
+      alert("購入に失敗しました");
+      return;
+    }
+    alert("購入しました");
+    this.router.navigate(["/"]);
   }
 
 }
