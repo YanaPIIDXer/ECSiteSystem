da00994fd5a1bd51b54f334171c8202172dc662d
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-10 07:58:39 +0900

    カートへの追加処理実装

--- frontend/src/app/Components/product-card/product-card.component.ts
+++ frontend/src/app/Components/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/Models/product';
+import conn from '../../Modules/apiconnection';
 
 @Component({
   selector: 'product-card',
@@ -20,7 +21,23 @@ export class ProductCardComponent implements OnInit {
   }
 
   async add(): Promise<void> {
-
+    if (this.count <= 0) {
+      alert("１個以上は追加してください。");
+      return;
+    }
+    if (!confirm("カートに入れますか？")) { return; }
+    var params = new URLSearchParams();
+    // ↓何故か文字列型しか受け付けないらしい
+    var id = this.product?.id.toString()!;
+    var count = this.count.toString()!;
+    params.append("id", id);
+    params.append("count", count);
+    const res = await conn.post("/user/cart/add", params);
+    if (res.status != 200 || !res.json.result) {
+      alert("追加に失敗しました。");
+      return;
+    }
+    alert("追加しました。");
   }
 
 }
