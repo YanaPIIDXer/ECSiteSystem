d26b1b27ce2458c0deeb70ca46cae90d9c065463
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-09 19:56:12 +0900

    テスト用カード一覧

--- frontend/src/app/Views/user-register/user-register.component.ts
+++ frontend/src/app/Views/user-register/user-register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface DummyCard {
+  name: string
+  number: string
+}
+
 @Component({
   selector: 'user-register',
   templateUrl: './user-register.component.html',
@@ -12,6 +17,7 @@ export class UserRegisterComponent implements OnInit {
   password: string
   confirmPassword: string
   address: string
+  dummyCards: DummyCard[]
 
   constructor() {
     this.name = "";
@@ -19,6 +25,17 @@ export class UserRegisterComponent implements OnInit {
     this.password = "";
     this.confirmPassword = "";
     this.address = "";
+    this.dummyCards =  [
+      {name: "VISA1", number: "4111111111111111"},
+      {name: "VISA2", number: "4242424242424242"},
+      {name: "VISA3", number: "4012888888881881"},
+      {name: "MasterCard1", number: "5555555555554444"},
+      {name: "MasterCard2", number: "5105105105105100"},
+      {name: "American Express1", number: "378282246310005"},
+      {name: "American Express2", number: "371449635398431"},
+      {name: "JCB1", number: "3530111333300000"},
+      {name: "JCB2", number: "3566002020360505"},
+    ];
   }
 
   ngOnInit(): void {
