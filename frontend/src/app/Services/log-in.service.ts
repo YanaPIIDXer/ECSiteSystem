8980c6fce5b962de525d75fa3ff6b0e1c6fee2d3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-09 15:57:46 +0900

    sessionStorageを使い、リロードしても状態が初期化されないようにした

--- frontend/src/app/Services/log-in.service.ts
+++ frontend/src/app/Services/log-in.service.ts
@@ -8,13 +8,19 @@ export class LogInService {
   private role: string
 
   constructor() {
-    this.role = "";
+    const role = sessionStorage.getItem("role");
+    this.role = role ? role : "";
   }
 
   isLogIn(): boolean { return this.role != ""; }
   isUser(): boolean { return this.role == "USER"; }
   isAdmin(): boolean { return this.role == "ADMIN"; }
-  logInAsUser(): void { this.role = "USER"; }
-  logInAsAdmin(): void { this.role = "ADMIN"; }
+  logInAsUser(): void { this.setRole("USER"); }
+  logInAsAdmin(): void {this.setRole("ADMIN"); }
   logOut(): void { this.role = ""; }
+
+  private setRole(role: string): void {
+    this.role = role;
+    sessionStorage.setItem("role", role);
+  }
 }
