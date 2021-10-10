ecbe193943ce96cfd1056a73e467d7031e7d5d2c
YanaPIIDXer <>
2021-10-10 10:57:10 +0900

    それぞれのロール以外のアクセスを弾くためのガードを定義

---
+++ frontend/src/app/Guards/admin-only.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LogInService } from '../Services/log-in.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminOnlyGuard implements CanActivate {
+  constructor(private logInService: LogInService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.logInService.isAdmin();
+  }
+  
+}
