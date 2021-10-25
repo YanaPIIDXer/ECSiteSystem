0c6eb501edbe947ce907c7df745df5d6672745fe
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-26 03:46:39 +0900

    画像が無い時は専用の画像を表示するようにした ついでに商品リストの画像サイズも統一するようにした

--- frontend/src/app/Components/item-image/item-image.component.ts
+++ frontend/src/app/Components/item-image/item-image.component.ts
@@ -1,17 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'item-image',
   templateUrl: './item-image.component.html',
   styleUrls: ['./item-image.component.css']
 })
-export class ItemImageComponent {
+export class ItemImageComponent implements OnInit {
   @Input() src: string
   @Input() forCard: boolean
 
   constructor() {
     this.src = "";
     this.forCard = false;
+  }
+  ngOnInit(): void {
+    if (this.src == "") {
+      this.src = "assets/noimage.jpg";
+    }
   }
 
 }
