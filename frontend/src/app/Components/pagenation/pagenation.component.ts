02c85c28b850c0fa1f1b8888b64b7820f276f1d0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-10-26 22:49:54 +0900

    ページネーションComponentの実装

--- frontend/src/app/Components/pagenation/pagenation.component.ts
+++ frontend/src/app/Components/pagenation/pagenation.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'pagenation',
   templateUrl: './pagenation.component.html',
   styleUrls: ['./pagenation.component.css']
 })
-export class PagenationComponent implements OnInit {
+export class PagenationComponent {
+  @Input() currentPage: number
+  @Input() maxPage: number
+  pageArray: Array<number>
+  @Output() click: EventEmitter<number>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    // ↓setupメソッドでやりたいんだけどコンストラクタ内で初期化しないとエラーになるみたい
+    this.currentPage = 1;
+    this.maxPage = 10;
+    this.pageArray = Array(this.maxPage);
+    this.click = new EventEmitter<number>();
   }
 
+  setup(currentPage: number, maxPage: number): void {
+    this.currentPage = currentPage;  
+    this.maxPage = maxPage;
+    this.pageArray = Array(this.maxPage);
+  }
+
+  onClick(page: number): void {
+    this.click.emit(page);
+  }
 }
