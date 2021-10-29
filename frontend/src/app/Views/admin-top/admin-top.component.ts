919fc958d80c0a73cb75d7f5b92d69396a79667b
YanaPIIDXer <>
2021-10-29 21:29:30 +0900

    サンプル商品を追加するAPIを叩くボタンを実装

--- frontend/src/app/Views/admin-top/admin-top.component.ts
+++ frontend/src/app/Views/admin-top/admin-top.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import conn from '../../Modules/apiconnection';
 
 @Component({
   selector: 'admin-top',
@@ -11,6 +12,19 @@ export class AdminTopComponent {
 
   constructor() {
     this.isProcessing = false;
+  }
+
+  async addSampleProducts(): Promise<void> {
+    if (!confirm("サンプル商品を追加しますか？")) { return; }
+    // 長時間かかる処理なのでブロッキング
+    this.isProcessing = true;
+    const res = await conn.post("/admin/product/add_samples", new URLSearchParams());
+    this.isProcessing = false;
+    if (res.status != 200 || !res.json.result) {
+      alert("追加できませんでした");
+      return;
+    }
+    alert("サンプル商品を追加しました");
   }
 
 }
